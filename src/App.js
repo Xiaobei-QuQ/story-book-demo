48f2357f56d7c207976e3c68263b811e94a4ccdc
north <xiaobei.quq@gmail.com>
2019-06-26 20:13:44 +0800

    taskbox UI

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './lib/redux';
 
-import TaskList from './components/TaskList';
+import InboxScreen from './components/InboxScreen';
 
 class App extends Component {
-  render() {
-    return (
-        <Provider store={store}>
-          <TaskList />
-        </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <InboxScreen />
+            </Provider>
+        );
+    }
 }
 
 export default App;
